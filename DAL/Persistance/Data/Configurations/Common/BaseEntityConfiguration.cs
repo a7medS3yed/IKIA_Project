29ee3c37bc9170cf4b2a6505c882f0daa1d28485
ahmed <sa8072750@gmail.com>
2025-05-01 17:45:58 +0300

    Configurations

---
+++ DAL/Persistance/Data/Configurations/Common/BaseEntityConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Entities.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DAL.Persistance.Data.Configurations.Common
+{
+    public class BaseEntityConfiguration<TKey, TModel> : IEntityTypeConfiguration<TModel>
+        where TModel : BaseEntity<TKey>
+        where TKey : IEquatable<TKey>
+    {
+        public virtual void Configure(EntityTypeBuilder<TModel> builder)
+        {
+            builder.HasKey(B => B.Id);
+            builder.Property(B => B.CreatedBy).HasMaxLength(50);
+            builder.Property(B => B.LastModifiedBy).HasMaxLength(50);
+            builder.Property(B => B.CreatedOn).HasDefaultValueSql("getdate()");
+            builder.Property(B => B.LastModifiedOn).HasComputedColumnSql("getdate()");
+        }
+    }
+
+}
---
+++ DAL/Persistance/Data/Configurations/Departments/DepartmentConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Entities.Common;
+using DAL.Entities.Departments;
+using DAL.Persistance.Data.Configurations.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DAL.Persistance.Data.Configurations.Departments
+{
+    public class DepartmentConfiguration : BaseEntityConfiguration<int, Department>
+    {
+        public override void Configure(EntityTypeBuilder<Department> builder)
+        {
+            base.Configure(builder);
+
+            builder.Property(D => D.Id).UseIdentityColumn(10, 10);
+            builder.Property(D => D.Name).HasMaxLength(50);
+            builder.Property(D => D.Code).HasMaxLength(10);
+            builder.Property(D => D.Description).HasMaxLength(100);
+            builder.Property(D => D.CreationDate).HasDefaultValueSql("GETDATE()");
+        }
+    }
+    
+}
