386d80d51932f61525650be6a1519d7ecf216015
ahmed <sa8072750@gmail.com>
2025-05-01 16:42:02 +0300

    Base and Department Module

---
+++ DAL/Entities/Common/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities.Common
+{
+    public class BaseEntity<TKey> where TKey : IEquatable<TKey>
+    {
+        public TKey Id { get; set; }
+        public required string CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public required string LastModifiedBy { get; set; }
+        public DateTime LastModifiedOn { get; set; }
+    }
+}
